cfb887df12f047abcbdbca6ade1cf27215e0ea69
Nobody Industries <nobodyindustries@gmail.com>
2025-03-17 12:32:07 +0200

    fix: CI/CD Pipeline

--- deploy/notify.sh
+++ deploy/notify.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 echo "Notifying Coolify via Webhook..."
-curl -v --fail-with-body --request GET "${COOLIFY_WEBHOOK}" --header "\'Authorization: Bearer ${COOLIFY_API_TOKEN}\'"
+curl --http1.1 --fail-with-body --request GET "${COOLIFY_WEBHOOK}" --header "\'Authorization: Bearer ${COOLIFY_API_TOKEN}\'"
