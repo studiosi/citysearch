699be6f6f210f8d6b73a7bebb58649a454cd8d99
Nobody Industries <nobodyindustries@gmail.com>
2025-03-17 11:57:41 +0200

    fix: CI/CD Pipeline

--- push_image.sh
+++ push_image.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+echo "Log into registry..."
 docker login --username "${GHCR_USERNAME}" --password "${GHRC_ACCESS_TOKEN}" ghcr.io
+echo "Build image..."
 docker compose build
+echo "Push image to registry..."
 docker push ghcr.io/studiosi/citysearch
